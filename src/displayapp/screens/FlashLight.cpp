718e5be08e532d041275767e36c30be485a154a5
Johey <joheyy@protonmail.com>
2022-06-12 00:48:06 +0200

    Set the default brightness of the flashlight to the highest setting
    In most cases you'll want the brightest possible setting when needing a flashlight.

--- src/displayapp/screens/FlashLight.cpp
+++ src/displayapp/screens/FlashLight.cpp
@@ -21,7 +21,8 @@ FlashLight::FlashLight(Pinetime::Applications::DisplayApp* app,
 {
   brightnessController.Backup();
 
-  brightnessLevel = brightnessController.Level();
+  brightnessLevel = Pinetime::Controllers::BrightnessController::Levels::High;
+  brightnessController.Set(brightnessLevel);
 
   flashLight = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_font(flashLight, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_sys_48);
